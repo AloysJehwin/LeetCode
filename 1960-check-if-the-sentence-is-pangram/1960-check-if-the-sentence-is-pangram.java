15229ce10651d4dadefcb94d3c15eabe4f355a6c
Aloys Jehwin <124761596+AloysJehwin@users.noreply.github.com>
2025-01-19 21:47:10 +0530

    Time: 2 ms (72.09%), Space: 41.7 MB (61.89%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkIfPangram(String s){
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
