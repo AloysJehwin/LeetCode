397d5bc7e92904573b6657eaa3c1daf06eb25ed3
Aloys Jehwin <124761596+AloysJehwin@users.noreply.github.com>
2025-03-25 09:23:25 +0530

    Time: 1 ms (76.81%), Space: 46.2 MB (17.72%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int maxProfit(int[] nums){
+        int maxProfit=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                maxProfit+=nums[i]-nums[i-1];
+            }
+        }
+        return maxProfit;
+    }
+} 
