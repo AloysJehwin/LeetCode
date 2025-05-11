d52f4ee5c06cd6dd1da3b5d2c4c038259e8514a7
Aloys Jehwin <124761596+AloysJehwin@users.noreply.github.com>
2025-05-11 22:04:47 +0530

    Time: 1 ms (37.34%), Space: 40.6 MB (95.98%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,8 +1,5 @@
 class Solution {
-    public static boolean isPowerOfTwo(int n){
-        if(n<=0) return false;
-
-        return (n&(n-1))==0;
+    public boolean isPowerOfTwo(int n) {
+        return n>0 && (n&(n-1))==0;
     }
-    
 }
