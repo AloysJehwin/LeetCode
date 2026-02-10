343884c3ac9156c3e14c693311ce1a392e2af52a
Aloys Jehwin <124761596+AloysJehwin@users.noreply.github.com>
2026-02-10 22:12:15 +0530

    Time: 1 ms (78.91%) | Memory: 44.9 MB (37.09%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n=nums.length;
+        int i=1;
+        while(i<n && nums[i]>nums[i-1]){
+            i++;
+        }
+        if(i==1 || i==n) return false;
+        while(i<n && nums[i]<nums[i-1]){
+            i++;
+        }
+        if(i==n) return false;
+        while(i<n && nums[i]>nums[i-1]){
+            i++;
+        }
+        return i==n;
+    }
+}
